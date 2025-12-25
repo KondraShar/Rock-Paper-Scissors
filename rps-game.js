ac8bad39f20efd6cd8ac32858e222279b1c33909
Kondra Shar <kondra.shar@pm.me>
2025-12-25 19:57:14 +0100

    Add 'getComputerChoice' func

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,20 @@
 console.log("hello there");
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3) + 1;
+    const choice = {1:"Rock", 2:"Paper", 3:"Scissors"};
+
+    switch(random) {
+        case 1:
+            return choice[1];
+        case 2:
+            return choice[2];
+        case 3:
+            return choice[3];
+        default:
+            return 0;
+    }
+    
+}
+
+console.log(getComputerChoice());
