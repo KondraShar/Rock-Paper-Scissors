694fb99aa51b3ea41103739dcd905a73b745cd25
Kondra Shar <kondra.shar@pm.me>
2025-12-25 20:15:28 +0100

    Refactor 'getComputerChoice' return

--- rps-game.js
+++ rps-game.js
@@ -4,17 +4,7 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
     const choice = {1:"Rock", 2:"Paper", 3:"Scissors"};
 
-    switch(random) {
-        case 1:
-            return choice[1];
-        case 2:
-            return choice[2];
-        case 3:
-            return choice[3];
-        default:
-            return 0;
-    }
-    
+    return choice[random];
 }
 
 function getHumanChoice() {
